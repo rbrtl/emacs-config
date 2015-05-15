0479657692dbff1071a59c19919f2a520273ef61
Gábor Oláh <olikas.g@gmail.com>
2015-05-15 10:51:27 +0200

    Some minor modifications added.

--- resources/conf-common.el
+++ resources/conf-common.el
@@ -3,3 +3,9 @@
 (tool-bar-mode -1)
 (scroll-bar-mode -1)
 
+;; ediff does not open a new frame
+(setq ediff-window-setup-function 'ediff-setup-windows-plain)
+
+;; Tab to spaces
+(setq tab-width 4)
+(setq-default indent-tabs-mode nil)
