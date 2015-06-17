ceaf4c2abd6ccbce2b2e241982d5f357b9b7c0b4
Gabor Olah <gabor.olah@erlang-solutions.com>
2015-06-17 10:44:50 +0200

    Ask before exit

--- resources/conf-common.el
+++ resources/conf-common.el
@@ -21,3 +21,16 @@
       user-full-name "Gabor Olah")
 
 (setq desktop-save-mode t)
+
+; Thanks to Niles (http://nileshk.com/2009/06/13/prompt-before-closing-emacs.html)
+(defun ask-before-closing ()
+  "Ask whether or not to close, and then close if y was pressed"
+  (interactive)
+  (if (y-or-n-p (format "Are you sure you want to exit Emacs? "))
+      (if (< emacs-major-version 22)
+          (save-buffers-kill-terminal)
+        (save-buffers-kill-emacs))
+    (message "Canceled exit")))
+
+(when window-system
+  (global-set-key (kbd "C-x C-c") 'ask-before-closing))
