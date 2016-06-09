4393b58f7c8de268aa51a48028f31ea40862912c
Gabor Olah <gabor.olah@erlang-solutions.com>
2016-06-09 15:33:30 +0100

    Make package archive work beyond proxies

--- config.el
+++ config.el
@@ -60,9 +60,9 @@
 (require 'package)
 
 (add-to-list 'package-archives
-             '("melpa-stable" . "https://stable.melpa.org/packages/") t)
-;;(add-to-list 'package-archives
-;;             '("melpa" . "http://melpa.milkbox.net/packages/") t)
+             '("melpa-stable" . "http://stable.melpa.org/packages/") t)
+(add-to-list 'package-archives
+             '("melpa" . "http://melpa.milkbox.net/packages/") t)
 (add-to-list 'package-archives
              '("elpa" . "http://elpa.gnu.org/packages/"))
 (add-to-list 'package-archives
