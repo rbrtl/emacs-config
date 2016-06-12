89605bc35b4716f2f0af4937bb2d82d87bf98c35
Gabor Olah <gabor.olah@erlang-solutions.com>
2016-06-12 18:11:52 +0100

    Flyspell settings (non-programming environment)

--- config.el
+++ config.el
@@ -222,6 +222,26 @@
 
 ;;;;
 
+;; find aspell and hunspell automatically
+(cond
+ ;; try hunspell at first
+  ;; if hunspell does NOT exist, use aspell
+ ((executable-find "hunspell")
+  (setq ispell-program-name "hunspell")
+  (setq ispell-local-dictionary "en_US")
+  (setq ispell-local-dictionary-alist
+        ;; Please note the list `("-d" "en_US")` contains ACTUAL parameters passed to hunspell
+        ;; You could use `("-d" "en_US,en_US-med")` to check with multiple dictionaries
+        '(("en_US" "[[:alpha:]]" "[^[:alpha:]]" "[']" nil ("-d" "en_US") nil utf-8)
+          )))
+
+ ((executable-find "aspell")
+  (setq ispell-program-name "aspell")
+  ;; Please note ispell-extra-args contains ACTUAL parameters passed to aspell
+  (setq ispell-extra-args '("--sug-mode=ultra" "--lang=en_US"))))
+
+(global-set-key (kbd "C-;") 'ispell-word)
+
 
 ;; (add-to-list 'load-path "resources")
 
