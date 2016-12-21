c209d207976496c6750958cea2427aa82a377e37
Gabor Olah <olikas.g@gmail.com>
2016-12-21 13:04:57 +0000

    Add scala support

--- config.el
+++ config.el
@@ -234,6 +234,25 @@
   :ensure t
   :pin melpa-stable)
 
+(use-package ensime
+  :ensure t
+  :pin melpa-stable
+  :config
+  (setq ensime-startup-snapshot-notification nil))
+
+(use-package scala-mode
+  :interpreter
+  ("scala" . scala-mode))
+
+(use-package sbt-mode
+  :commands sbt-start sbt-command
+  :config
+  ;; WORKAROUND: https://github.com/ensime/emacs-sbt-mode/issues/31
+  ;; allows using SPACE when in the minibuffer
+  (substitute-key-definition
+   'minibuffer-complete-word
+   'self-insert-command
+   minibuffer-local-completion-map))
 ;;;;
 
 ;; find aspell and hunspell automatically
